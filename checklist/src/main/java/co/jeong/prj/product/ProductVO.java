3bacfb4d0eb5c0d24e2376411cc2959478adfe81
admin <admin@YD02-22>
2021-10-19 18:27:20 +0900

    2021 -10 -19 깃버그

--- checklist/src/main/java/co/jeong/prj/product/ProductVO.java
+++ checklist/src/main/java/co/jeong/prj/product/ProductVO.java
@@ -1,6 +1,10 @@
 package co.jeong.prj.product;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class ProductVO {
 	
 	private String productcompany;
@@ -10,48 +14,5 @@ public class ProductVO {
 	private int productprices;
 	private int productclass;
 	private int productstock;
-	
-	public String getProductcompany() {
-		return productcompany;
-	}
-	public void setProductcompany(String productcompany) {
-		this.productcompany = productcompany;
-	}
-	public String getProductkind() {
-		return productkind;
-	}
-	public void setProductkind(String productkind) {
-		this.productkind = productkind;
-	}
-	public String getProductname() {
-		return productname;
-	}
-	public void setProductname(String productname) {
-		this.productname = productname;
-	}
-	public int getProductnum() {
-		return productnum;
-	}
-	public void setProductnum(int productnum) {
-		this.productnum = productnum;
-	}
-	public int getProductprices() {
-		return productprices;
-	}
-	public void setProductprices(int productprices) {
-		this.productprices = productprices;
-	}
-	public int getProductclass() {
-		return productclass;
-	}
-	public void setProductclass(int productclass) {
-		this.productclass = productclass;
-	}
-	public int getProductstock() {
-		return productstock;
-	}
-	public void setProductstock(int productstock) {
-		this.productstock = productstock;
-	}
 	
 }
